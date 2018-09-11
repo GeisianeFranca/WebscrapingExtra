787a73688b6d8d56c5ae24b72383d6af8c038d08
GeisianeFranca <geisianefranca@gmail.com>
2018-09-10 23:08:20 -0300

    Código refatorado

--- Extra_produtos.py
+++ Extra_produtos.py
@@ -18,8 +18,13 @@ class Produto(object):
             passwd="12345",
             database="test"
         )
+        self.list_tipo = ["Parboilizado", "Agulhinha", "Integral", "Risoto", "Orgânico", "Basmati", "Carioca", "Preto",
+                          "Branco", "Vermelho", "Fradinho"]
+        self.list_fabricante = ["URBANO", "TIO JOÃO", "PASTAROTTI", "BIJU", "PANTERA", "CAMIL", "LA PASTINA", "RÁRIS", "BLUE VILLE",
+                                "CASINO", "QUALITÁ", "PRATO FINO", "PILECCO NOBRE", "PILECO NOBRE", "Organic", "KI CALDO",
+                                "SUPER MÁXIMO", "SUPER MAXIMO", "YOKI", "VAPZA"]
+        self.list_subcategoria = ["arroz", "feijao"]
         self.cursor = self.db.cursor(buffered=True)
-        #self.cursor = self.db.cursor()
 
     def set_URL(self, URL):
         self.URL = URL
@@ -47,11 +52,10 @@ class Produto(object):
         return product_list
 
     def def_subcategoria(self):
-        self.subcategoria = ""
-        if self.URL.find("arroz") != -1:
-            self.subcategoria = "Arroz"
-        elif self.URL.find("feijao") != -1:
-            self.subcategoria = "Feijão"
+        self.subcategoria = "Outro"
+        for i in self.list_subcategoria:
+            if str(self.URL).find(i) != -1:
+                self.subcategoria = i
         return self.subcategoria
 
     def def_peso(self):
@@ -62,78 +66,24 @@ class Produto(object):
         return self.peso
 
     def def_tipo(self):
-        self.tipo = ""
-        if str(self.nome).find("Parboilizado") != -1:
-            self.tipo = "Parboilizado"
-        elif str(self.nome).find("Agulhinha") != -1:
-            self.tipo = "Agulhinha"
-        elif str(self.nome).find("Integral") != -1:
-            self.tipo = "Integral"
-        elif str(self.nome).find("Risoto") != -1:
-            self.tipo = "Risoto"
-        elif str(self.nome).find("Orgânico") != -1:
-            self.tipo = "Orgânico"
-        elif str(self.nome).find("Basmati") != -1:
-            self.tipo = "Basmati"
-        elif str(self.nome).find("Carioca") != -1:
-            self.tipo = "Carioca"
-        elif str(self.nome).find("Preto") != -1:
-            self.tipo = "Preto"
-        elif str(self.nome).find("Branco") != -1:
-            self.tipo = "Branco"
-        elif str(self.nome).find("Vermelho") != -1:
-            self.tipo = "Vermelho"
-        elif str(self.nome).find("Fradinho") != -1:
-            self.tipo = "Fradinho"
-        else:
-            self.tipo = "Outros"
+        self.tipo = "Outro"
+        for i in self.list_tipo:
+            if str(self.nome).find(i) != -1:
+                self.tipo = i
         return self.tipo
 
-    def def_fabricante(self, nome):
-        self.fabricante=""
-        if str(nome).find("URBANO") != -1:
-            self.fabricante = "URBANO"
-        elif str(nome).find("TIO JOÃO") != -1:
-            self.fabricante = "TIO JOÃO"
-        elif str(nome).find("PASTAROTTI") != -1:
-            self.fabricante = "PASTAROTTI"
-        elif str(nome).find("BIJU") != -1:
-            self.fabricante = "BIJU"
-        elif str(nome).find("PANTERA") != -1:
-            self.fabricante = "PANTERA"
-        elif str(nome).find("CAMIL") != -1:
-            self.fabricante = "CAMIL"
-        elif str(nome).find("LA PASTINA") != -1:
-            self.fabricante = "LA PASTINA"
-        elif str(nome).find("RÁRIS") != -1:
-            self.fabricante = "RÁRIS"
-        elif str(nome).find("CASINO") != -1:
-            self.fabricante = "CASINO"
-        elif str(nome).find("BLUE VILLE") != -1:
-            self.fabricante = "BLUE VILLE"
-        elif str(nome).find("QUALITÁ") != -1:
-            self.fabricante = "QUALITÁ"
-        elif str(nome).find("PRATO FINO") != -1:
-            self.fabricante = "PRATO FINO"
-        elif str(nome).find("Organic") != -1:
-            self.fabricante = "Organic"
-        elif str(nome).find("PILECCO NOBRE") != -1 or str(nome).find("PILECO NOBRE") != -1:
-            self.fabricante = "PILECCO NOBRE"
-        elif str(nome).find("KI CALDO") != -1:
-            self.fabricante = "KI CALDO"
-        elif (str(nome).find("SUPER MÁXIMO") != -1 or str(nome).find("SUPER MAXIMO") != -1):
-            self.fabricante = "SUPER MÁXIMO"
-        elif str(nome).find("YOKI") != -1:
-            self.fabricante = "YOKI"
-        elif str(nome).find("VAPZA") != -1:
-            self.fabricante = "VAPZA"
+    def def_fabricante(self):
+        self.fabricante = "Outro"
+        for i in self.list_fabricante:
+            if str(self.nome).find(i) != -1:
+                self.fabricante = i
         return self.fabricante
 
     def list_prod(self, product_list):
         for product in product_list:
             self.nome = product['produto-nome']
             self.preco = product['produto-preco']
-            self.fabricante = self.def_fabricante(self.nome)
+            self.fabricante = self.def_fabricante()
             self.p_qtd = product['produto-qtd']
             p_sku = product['produto-sku']
             self.p_ruptura = product['ruptura']
@@ -142,9 +92,9 @@ class Produto(object):
                 print("=========================")
                 print("Nome: %s" %(self.nome))
                 print("Preco: %s" %(self.preco))
-                print("Fabricante: %s" % (self.def_fabricante(self.nome)))
+                print("Fabricante: %s" % (self.def_fabricante()))
                 print("Quantidade: %s" % (self.p_qtd))
-                print("Subcategoria: %s" % (self.subcategoria))
+                print("Subcategoria: %s" % (self.def_subcategoria()))
                 print("Tipo: %s" % (self.def_tipo()))
                 print("Peso: %s" % (self.def_peso()))
                 self.cursor.execute("SELECT * from Produtos where Nome ='" + self.nome + "' and IdMercado = 4")
@@ -155,7 +105,7 @@ class Produto(object):
                     self.cursor.execute(
                         "INSERT INTO Produtos (Nome, Categoria, Subcategoria, Fabricante, Preco, Peso, Quantidade, IdMercado, tipo)"
                         " VALUES ('" + str(self.nome) + "', 'Alimentos', '" + str(self.def_subcategoria()) + "', '" + str(
-                            self.def_fabricante(self.nome)) + "', '" + str(self.preco) + "','" + str(self.def_peso()) + "','" + str(
+                            self.def_fabricante()) + "', '" + str(self.preco) + "','" + str(self.def_peso()) + "','" + str(
                             self.p_qtd) + "', 4, '" + str(self.def_tipo()) + "')")
 
                 self.db.commit()
